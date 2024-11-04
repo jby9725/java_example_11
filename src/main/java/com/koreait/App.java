6dbec47f2d0d8696bd650432f31add15cbc20c24
jby9725 <jby9725@gmail.com>
2024-11-04 14:32:09 +0900

    todo CRUD 구현

--- src/main/java/com/koreait/App.java
+++ src/main/java/com/koreait/App.java
@@ -1,59 +1,70 @@
 package com.koreait;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
-
-    Scanner in = new Scanner(System.in);
-
     public void run() {
-        String cmd = "";
+        System.out.println("Todo App 시작");
 
-        int lastId = 0;
-        Map<Integer, String> list = new HashMap<>();
+        try (Scanner sc = new Scanner(System.in)) {
+            List<Todo> todos = new ArrayList<>();
+            long todosLastId = 0;
 
-        while (true) {
-            System.out.print("명령) ");
-            cmd = in.nextLine();
+            while (true) {
+                System.out.print("명령) ");
+                String cmd = sc.nextLine().trim();
 
-            if (cmd.equals("exit"))
-                break;
+                if (cmd.equals("exit")) break;
+                else if (cmd.equals("add")) {
+                    long id = todosLastId + 1;
+                    System.out.print("할 일 : ");
+                    String content = sc.nextLine().trim();
 
-            else if (cmd.equals("add")) {
-                System.out.print("할일 : ");
-                String todo = in.nextLine();
-                lastId++;
-                list.put(lastId, todo);
-                System.out.printf("%d번 할일이 생성되었습니다.\n", lastId);
+                    Todo todo = new Todo(id, content);
+                    todos.add(todo);
+                    todosLastId++;
 
-            } else if (cmd.equals("list")) {
-                System.out.println("번호 / 내용");
+                    System.out.printf("%d번 todo가 생성되었습니다\n", id);
+                } else if (cmd.equals("list")) {
+                    System.out.println("번호   /   내용  ");
 
-                // 키 값을 ArrayList에 담아 정렬
-                ArrayList<Integer> keys = new ArrayList<>(list.keySet());
-                Collections.sort(keys);
+                    todos.forEach(todo -> System.out.printf("%d  /  %s  \n", todo.getId(), todo.getContent()));
+                } else if (cmd.equals("del")) {
+                    System.out.printf("삭제할 할일의 번호 : ");
+                    long id = Long.parseLong(sc.nextLine().trim());
 
-                // 정렬된 키 순서대로 값 출력
-                for (Integer key : keys) {
-                    System.out.printf("%d / %s\n", key, list.get(key));
+                    boolean isRemoved = todos.removeIf(todo -> todo.getId() == id);
+
+                    if (!isRemoved) {
+                        System.out.printf("%d번 할일은 존재하지 않습니다.\n", id);
+                        continue;
+                    }
+                    System.out.printf("%d번 할일이 삭제되었습니다.\n", id);
+                } else if (cmd.equals("modify")) {
+                    System.out.printf("수정할 할일의 번호 : ");
+                    long id = Long.parseLong(sc.nextLine().trim());
+
+                    Todo foundTodo = todos.stream()
+                            .filter(t -> t.getId() == id)
+                            .findFirst()
+                            .orElse(null);
+
+                    if (foundTodo == null) {
+                        System.out.printf("%d번 할일은 존재하지 않습니다.\n", id);
+                        continue;
+                    }
+
+                    System.out.printf("기존 할일 : %s\n", foundTodo.getContent());
+                    System.out.print("새 할일 : ");
+                    foundTodo.setContent(sc.nextLine().trim());
+
+                    System.out.printf("%d번 할일이 수정되었습니다.\n", id);
                 }
-
-            } else if (cmd.equals("del")) {
-                System.out.print("삭제할 할일의 번호 : ");
-                int id = Integer.parseInt(in.nextLine());
-                list.remove(id);
-
-            } else if (cmd.equals("modify")) {
-                System.out.print("수정할 할일의 번호 : ");
-                int id = Integer.parseInt(in.nextLine());
-                System.out.println("기존 할 일 : " + list.get(id));
-                System.out.print("새 할 일 : ");
-                String todo = in.nextLine();
-                list.put(id, todo);
-                System.out.printf("%d번 할 일이 수정되었습니다.\n", id);
-
             }
         }
 
+        System.out.println("Todo App 끝");
     }
 }
